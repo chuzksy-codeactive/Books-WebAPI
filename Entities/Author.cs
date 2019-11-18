1a1734f00d053011ccb7e62742c4b99988cc9849
onuchukwuchika <chuzksy@gmail.com>
2019-11-18 11:37:23 +0100

    Add DbContext and seed data into the tables

--- Entities/Author.cs
+++ Entities/Author.cs
@@ -2,7 +2,7 @@ using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Book.Api.Entities
+namespace Books.Api.Entities
 {
     [Table ("Authors")]
     public class Author
--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Book.Api
+namespace Books.Api
 {
     public class Program
     {
